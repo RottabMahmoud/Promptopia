53055a928ffc5042a34e2412f1a41fe0804681af
Rottab <totundberuhmt@gmail.com>
2023-12-24 19:47:38 +0200

    NavBar: 1. Authentication, Sign In & Sign Out with Google Authentication.

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,21 +1,28 @@
 import "@styles/globals.css";
 
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
+
 export const metadata = {
   title: "Promptopia",
   description: "Discover & Share AI Prompts",
 };
 
-const RootLayout = ({ children }) => {
-  return (
-    <html lang="en">
-      <body>
+const RootLayout = ({ children }) => (
+  <html lang="en">
+    <body>
+      <Provider>
         <div className="main">
-          <div className="gradient"></div>
+          <div className="gradient" />
         </div>
-        <main className="app">{children}</main>
-      </body>
-    </html>
-  );
-};
+
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
+      </Provider>
+    </body>
+  </html>
+);
 
 export default RootLayout;
